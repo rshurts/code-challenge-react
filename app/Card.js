0cdaf0b2d093944fd43a5292f315970cb77f300b
Russell Shurts <shurts@pythian.com>
2016-06-23 14:51:27 -0500

    Update style

--- app/Card.js
+++ app/Card.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
-import { Button, ButtonToolbar, Panel } from 'react-bootstrap';
+import { Button, ButtonToolbar, Collapse, Image, Label, Media, Panel, Glyphicon, Well } from 'react-bootstrap';
 
 class Card extends Component {
   constructor() {
@@ -16,21 +16,24 @@ class Card extends Component {
   }
 
   render() {
-    let cardMetadata;
-    if (this.state.showMetadata) {
-      cardMetadata = (
-        <div className="card-metadata">
-          <img src={this.props.actor_avator} alt={this.props.actor_username} />
-          <ul>
-            <li>Name: {this.props.actor_name}</li>
-            <li>Description: {this.props.actor_description}</li>
-            <li>Username: {this.props.actor_username}</li>
-            <li>Profile: <a href={this.props.actor_url}>{this.props.actor_url}</a></li>
-            <li>Location: {this.props.activity_longitude} {this.props.activity_latitude}</li>
-          </ul>
-        </div>
-      );
-    }
+    let cardMetadata = (
+      <div>
+        <Well>
+          <Media>
+            <Media.Left>
+              <Image src={this.props.actor_avator} alt={this.props.actor_username} height="160" width="160" />
+            </Media.Left>
+            <Media.Body>
+              <p><strong>Name:</strong> {this.props.actor_name}</p>
+              <p><strong>Description:</strong> {this.props.actor_description}</p>
+              <p><strong>Username:</strong> {this.props.actor_username}</p>
+              <p><strong>Profile:</strong> <a href={this.props.actor_url}>{this.props.actor_url}</a></p>
+              <p><strong>Location:</strong> {this.props.activity_longitude} {this.props.activity_latitude}</p>
+            </Media.Body>
+          </Media>
+        </Well>
+      </div>
+    );
 
     let activityMessage;
     if (this.props.activity_attachment_type) {
@@ -38,7 +41,7 @@ class Card extends Component {
         <img src={this.props.activity_attachment} alt={this.props.activity_attachment} />
       );
     } else {
-      activityMessage = (<p>{this.props.activity_message}</p>);
+      activityMessage = (<h3>{this.props.activity_message}</h3>);
     }
 
     let titleSentiment;
@@ -61,26 +64,21 @@ class Card extends Component {
         <Panel header={titleSentiment} bsStyle={titleColor}>
           <div>
             {activityMessage}
-            <p>
+            <h4>
               Posted by <a href={this.props.actor_url}>{this.props.actor_name}</a> {' '}
-              on <a href={this.props.activity_url}>{this.props.provider}</a> {' '}
-              on {this.props.activity_date}.
-            </p>
-          </div>
-
-          <div>
-            <ul>
-              <li>&#10084; {this.props.activity_likes}</li>
-              <li>&#10150; {this.props.activity_shares}</li>
-              <li>&#9998; {this.props.activity_comments}</li>
-            </ul>
+              on {this.props.activity_date} {' '}
+              to <a href={this.props.activity_url}>{this.props.provider}</a>. {' '}
+              <Label bsStyle="info"><Glyphicon glyph="heart" /> {this.props.activity_likes}</Label> {' '}
+              <Label bsStyle="info"><Glyphicon glyph="share-alt" /> {this.props.activity_shares}</Label> {' '}
+              <Label bsStyle="info"><Glyphicon glyph="comment" /> {this.props.activity_comments}</Label> {' '}
+            </h4>
           </div>
 
           <div>
             <ButtonToolbar>
-              <Button>&#10084; Like</Button>
-              <Button>&#10150; Share</Button>
-              <Button>&#9998; Comment</Button>
+              <Button><Glyphicon glyph="heart" /> Like</Button>
+              <Button><Glyphicon glyph="share-alt" /> Share</Button>
+              <Button><Glyphicon glyph="comment" /> Comment</Button>
               <Button
                 bsStyle="info"
                 onClick={this.toggleMetadata.bind(this)}
@@ -89,14 +87,18 @@ class Card extends Component {
               </Button>
             </ButtonToolbar>
           </div>
-
+          <Collapse in={this.state.showMetadata}>
+            {cardMetadata}
+          </Collapse>
+          {/*
           <ReactCSSTransitionGroup
             transitionName="toggle"
             transitionEnterTimeout={300}
             transitionLeaveTimeout={300}
           >
             {cardMetadata}
-          </ReactCSSTransitionGroup>
+            </ReactCSSTransitionGroup>
+            */}
         </Panel>
       </div>
     );
